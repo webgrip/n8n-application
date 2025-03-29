0c081c3bd4c4e18b86a80ce8eb78b40de0e92716
Ryan Grippeling <R.Grippeling@hotmail.com>
2025-03-29 11:52:14 +0100

    fix: Fixed credentials files

--- ops/docker/n8n/scripts/import-credentials.sh
+++ ops/docker/n8n/scripts/import-credentials.sh
@@ -9,17 +9,17 @@ mkdir -p /data/data/credentials
 # Ensure correct filenames are used
 if [ -f "/data/credentials/openapi.json" ]; then
     echo "Updating openapi.json..."
-    jq --arg apiKey "$OPENAI_API_KEY" '.data = $apiKey' /data/credentials/openapi.json > tmp && mv tmp /data/credentials/openapi.json
+    jq --arg apiKey "$OPENAI_API_KEY" '.data.apiKey = $apiKey' /data/credentials/openapi.json > tmp && mv tmp /data/credentials/openapi.json
 else
     echo "Warning: openapi.json not found, skipping..."
 fi
 
 if [ -f "/data/credentials/telegram.json" ]; then
     echo "Updating telegram.json..."
-    jq --arg apiKey "$TELEGRAM_API_KEY" '.data = $apiKey' /data/credentials/telegram.json > tmp && mv tmp /data/credentials/telegram.json
+    jq --arg apiKey "$TELEGRAM_API_KEY" '.data.accessToken = $apiKey' /data/credentials/telegram.json > tmp && mv tmp /data/credentials/telegram.json
 else
     echo "Warning: telegram.json not found, skipping..."
 fi
 
 # Import credentials into n8n
-N8N_ENCRYPTION_KEY=$N8N_ENCRYPTION_KEY n8n import:credentials --separate --input=/data/credentials
+n8n import:credentials --separate --input=/data/credentials
